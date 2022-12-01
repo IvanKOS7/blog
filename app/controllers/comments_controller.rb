71917aec925e4a93a8a8aafa4d1bb86dda1eb055
IvanKOS7 <ivankos429@gmail.com>
2022-12-02 02:00:33 +0700

    work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+ before_action :find_post
+
+  def create
+    comment = @post.comments.create!(params.required(:comment).permit(:content))
+  end
+
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
 end
